44dd0f1d04ad9d780c3f6257ffc03015c9be6b0c
Rizky <isky9352@gmail.com>
2024-11-05 15:13:34 +0800

    Add files via upload

---
+++ tugas 5.py
@@ -0,0 +1,12 @@
+print("#Tugas 5 karya Muhammad Rizki")
+
+print("nama saya = 24241180")
+
+print("nimAwal = 24")
+print("nimAkhir = 80")
+
+print("panjang = nimAkhir")
+print("lebar = nimAwal")
+
+print("luas=panjang * lebar")
+print("mencetak(f" luas dari{panjang}X{lebar}adalah = {luas)")
