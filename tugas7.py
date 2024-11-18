493794877b7ae98c069aaaa754b730323d4efc87
Rizky <isky9352@gmail.com>
2024-11-18 21:40:59 +0800

    Add files via upload

---
+++ tugas7.py
@@ -0,0 +1,9 @@
+# menghitung luas persegi
+# nama : muhammad rizki
+
+print("menghitung luas persegi")
+panjang = ke dalam(masukan("masukkan panjang : ")
+lebar = ke dalam(masukan("masukkan lebar : ")
+
+hasil = panjang * lebar
+print(f"hasil dari{panjang}X{lebar}={hasil}")
