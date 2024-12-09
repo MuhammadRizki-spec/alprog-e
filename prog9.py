36647cdefc80e8d3df470c1babda1cb4b74023ee
Rizky <isky9352@gmail.com>
2024-12-09 08:54:14 +0800

    Add files via upload

---
+++ prog9.py
@@ -0,0 +1,12 @@
+#Latihan Aritmatika
+
+nilai = float(input("masukkan nilai dalam suhu celcius : "))
+
+fahreinheit = (nilai * 9/5) + 32
+print("nilai dalam fahreinheit =",fahrenheit)
+
+kelvin = nilai + 273,151
+print("nilai dalam kelvin =",kelvin) 
+
+reamur = nilai * (5/4) + 0 
+print("nilai dalam reamur = ",reamur)
